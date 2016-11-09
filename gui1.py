ce5e7306383df3305be43dad2e32f1d082acd190
Aishwarya <aishwaryasinghal228@gmail.com>
2016-11-09 22:42:04 +0530

    test commit

--- gui1.py
+++ gui1.py
@@ -1,47 +1,62 @@
-from tkinter import *
+from Tkinter import *
+from databse import insert
+
 
 def raise_fr(fr):
-	fr.tkraise()
+    print 'raising'+str(fr)
+    fr.tkraise()
 
 top = Tk()
-top.geometry("175x200")
-frmain=Frame(top)
+top.geometry("175x300")
+fr_main = Frame(top)
 frReg = Frame(top)
 frSign = Frame(top)
 
-for frame in (frmain,frReg,frSign):
+for frame in (fr_main, frReg, frSign):
     frame.grid(row=0, column=0, sticky='news')
 
-#MAIN WINDOW
-buttReg = Button(frmain,text="Register",command=lambda:raise_fr(frReg)).pack()
-buttSign = Button(frmain,text="Sign in",command=lambda:raise_fr(frSign)).pack()
+# MAIN WINDOW
+buttReg = Button(fr_main, text="Register", command=lambda: raise_fr(frReg)).pack()
+buttSign = Button(fr_main, text="Sign in", command=lambda: raise_fr(frSign)).pack()
 
-#REGISTER WINDOW
-labName = Label(frReg,text="User Name").pack()
-entName = Entry(frReg).pack()
+# REGISTER WINDOW
+labName = Label(frReg, text="User Name").pack()
+entName = Entry(frReg)
+entName.pack()
 
-labEmail = Label(frReg,text="Email ID").pack()
-entEmail = Entry(frReg).pack()
+labEmail = Label(frReg, text="Email ID").pack()
+entEmail = Entry(frReg)
+entEmail.pack()
 
-labPass = Label(frReg,text="Password").pack()
-entPass = Entry(frReg).pack()
+labPass = Label(frReg, text="Password").pack()
+entPass = Entry(frReg)
+entPass.pack()
 
-mbCol =  Menubutton (frReg, text="Color", relief=RAISED )
-mbCol.menu  =  Menu (mbCol,tearoff = 0 )
-mbCol["menu"]  =  mbCol.menu
-varBlue  = IntVar()
-varRed = IntVar()
-mbCol.menu.add_checkbutton (label="Blue",variable=varBlue )
-mbCol.menu.add_checkbutton (label="Red",variable=varRed )
+labCol = Label(frReg, bg="white")
+mbCol = Menubutton(frReg, text="Color", relief=RAISED)
+mbCol.menu = Menu(mbCol, tearoff=0)
+mbCol["menu"] = mbCol.menu
+
+Color=None
+def opt(item):
+    Color=item
+    labCol.configure(bg=item)
+
+mbCol.menu.add_command(label="Blue", command=lambda: opt("Blue"))
+mbCol.menu.add_command(label="Red", command=lambda: opt("Red"))
 mbCol.pack()
-labCol = Label(frReg,bg="white")
+labCol.pack()
 
-buttSub1 = Button(frReg,text="SUBMIT").pack()
-buttmain1 = Button(frReg,text="BACK",command=lambda:raise_fr(frmain)).pack()
 
-#SIGN IN WINDOW
-buttSub2 = Button(frSign,text="SUBMIT").pack()
-buttmain2 = Button(frSign,text="BACK",command=lambda:raise_fr(frmain)).pack()
+def submit():
+    insert(entName.get(), entEmail.get(), entPass.get(), Color)
 
-raise_fr(frmain)
+buttSub1 = Button(frReg, text="SUBMIT", command=submit).pack()
+butt_main1 = Button(frReg, text="BACK", command=lambda: raise_fr(fr_main)).pack()
+
+# SIGN IN WINDOW
+buttSub2 = Button(frSign, text="SUBMIT").pack()
+butt_main2 = Button(frSign, text="BACK", command=lambda: raise_fr(fr_main)).pack()
+
+raise_fr(fr_main)
 top.mainloop()
